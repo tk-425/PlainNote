89a67ebd02e274288485a630f9ce2f87588fc725
trry <54043933+tk-425@users.noreply.github.com>
2023-04-14 03:57:49 -0400

    update: front-end update

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,10 +1,18 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {auth} from '../../utils/firebase';
 import './Footer.css';
 
 const Footer = () => {
+  const [user] = useAuthState(auth);
+
   return (
-    <div className='footer__container max-width text-center'>
-      <div>Footer</div>
-    </div>
+    <>
+      {!user && (
+        <footer className='footer__container max-width text-center'>
+          <div>Footer</div>
+        </footer>
+      )}
+    </>
   );
 };
 
--- frontend/src/components/main/main-content/MainContent.jsx
+++ frontend/src/components/main/main-content/MainContent.jsx
@@ -3,7 +3,8 @@ import './MainContent.css';
 const MainContent = () => {
   return (
     <div className='main__content'>
-      <h3>Content</h3>
+      <div className='viewer__container'>Viewer</div>
+      <div className='editor__container'>Editor</div>
     </div>
   );
 };
